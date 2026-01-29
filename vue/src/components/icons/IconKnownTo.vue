77214ba20a60ab205ee78bbda95fdabc7a79cc9f
Dave Berendhuysen <d.berendhuysen@gmail.com>
2026-01-29 11:18:59 +0100

    added icons known_to and trash

---
+++ vue/src/components/icons/IconKnownTo.vue
@@ -0,0 +1,49 @@
+<template>
+<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 88.97 65.61">
+	<g id="Layer_1-2" data-name="Layer 1">
+		<g>
+			<g>
+				<path class="cls-2" d="M45.47,46.44h0c10.58,0,19.17,8.59,19.17,19.17H26.3c0-10.58,8.59-19.17,19.17-19.17Z"/>
+				<circle class="cls-2" cx="45.47" cy="33.9" r="9.99"/>
+			</g>
+			<g>
+				<path class="cls-2" d="M69.8,39.8h0c10.58,0,19.17,8.59,19.17,19.17h-38.34c0-10.58,8.59-19.17,19.17-19.17Z"/>
+				<circle class="cls-2" cx="69.8" cy="27.27" r="9.99"/>
+			</g>
+			<g>
+				<path class="cls-2" d="M19.17,41.48h0c10.58,0,19.17,8.59,19.17,19.17H0C0,50.07,8.59,41.48,19.17,41.48Z"/>
+				<circle class="cls-2" cx="19.17" cy="28.95" r="9.99"/>
+			</g>
+			<g>
+				<rect class="cls-2" x="13.03" y="5.64" width="12.29" height="15.24"/>
+				<rect class="cls-2" x="8.19" y="20.88" width="21.96" height="1.47"/>
+			</g>
+			<g>
+				<path class="cls-3" d="M72.07,19.11l6.62,11.54.28.47c2.17-1.69,2.87-3.47,2.87-6.05,0-5.16-5.39-9.34-12.04-9.34s-12.04,4.18-12.04,9.34c0,2.52.92,4.37,3.01,6.05l.36-.36,10.81-11.44.13-.22Z"/>
+				<line class="cls-3" x1="72.07" y1="19.11" x2="69.87" y2="15.74"/>
+			</g>
+		</g>
+		<text class="cls-1" transform="translate(42.2 21.62)"><tspan x="0" y="0">!</tspan></text>
+	</g>
+</svg>
+</template>
+
+<style scoped>
+.cls-1 {
+	font-family: Inter-BlackItalic, Inter;
+	font-size: 25.03px;
+	font-style: italic;
+	font-variation-settings: 'wght' 900, 'slnt' -10;
+	font-weight: 800;
+}
+
+.cls-1, .cls-2 {
+	fill: #231f20;
+}
+
+.cls-3 {
+	fill: #fff;
+	stroke: #231f20;
+	stroke-miterlimit: 10;
+}
+</style>
